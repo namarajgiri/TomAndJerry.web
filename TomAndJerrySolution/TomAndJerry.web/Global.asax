c3055e4d8cc1e0237113d62cd1b07c4443d15867
namarajgiri <namarajgiri68@gmail.com>
2015-06-26 10:28:28 +0545

    First commit

---
+++ TomAndJerrySolution/TomAndJerry.web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TomAndJerry.web.MvcApplication" Language="C#" %>
---
+++ TomAndJerrySolution/TomAndJerry.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TomAndJerry.web.Startup))]
+namespace TomAndJerry.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
